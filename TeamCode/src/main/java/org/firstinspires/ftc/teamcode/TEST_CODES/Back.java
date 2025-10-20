ae89017d7eb3e2e608e7a9fe169f6a46f402a02d
loarado <brauliokoalas@gmail.com>
2025-10-20 16:08:10 -0500

    added testing autos

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TEST_CODES/Back.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.TEST_CODES;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+@Autonomous(name = "EasyTest", group = "Autonomous")
+public class Back extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        Pose2d beginPose = new Pose2d(0, -20, Math.toRadians(0));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+
+        waitForStart();
+
+            if (isStopRequested()) return;
+
+            Actions.runBlocking(
+                    drive.actionBuilder(beginPose)
+                            .strafeTo(new Vector2d(-62, -20))
+
+
+                            .build());
+
+    }
+}
